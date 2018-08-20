2ab4cbe77f34536f5b5f583c783e0a2a387cadb9
Peter <p.pan48711@gmail.com>
2018-08-20 11:53:52 -0400

    Update build script to point to correct cert file

--- build-tools/docker/shim-build/build.sh
+++ build-tools/docker/shim-build/build.sh
@@ -2,7 +2,7 @@
 set -x
 cd /workspace/cisco_common_shim
 id
-make VENDOR_CERT_FILE=build-tools/certs/trca.der
+make VENDOR_CERT_FILE=build-tools/certs/cisco_virtual_root.der
 mkdir -p install_path
 export DATATARGETDIR=$PWD/install_path
 make install-as-data
