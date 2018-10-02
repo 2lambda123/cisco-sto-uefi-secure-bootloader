3d8f0e83799d50559c7123f79b0354b2a670d25d
Peter <p.pan48711@gmail.com>
2018-10-02 11:23:27 -0400

    Add additional hash and version outputs

--- build-tools/docker/shim-build/build.sh
+++ build-tools/docker/shim-build/build.sh
@@ -2,9 +2,31 @@
 set -x
 cd /workspace/cisco_common_shim
 id
+# Dump out various versions
+gcc -v
+objcopy -V
+
 make VENDOR_CERT_FILE=build-tools/certs/cisco_virtual_root.der
 mkdir -p install_path
-export DATATARGETDIR=$PWD/install_path
-make install-as-data
+
+# Copy over the desired binaires and take the SHA512 hash
+cp shimx64.efi install_path/.
+cp mmx64.efi install_path/.
+cp fbx64.efi install_path/.
+
+# Output various hashes for integrity checking
+openssl dgst -sha512 install_path/shimx64.efi
+openssl dgst -sha512 install_path/mmx64.efi
+openssl dgst -sha512 install_path/fbx64.efi
+
+openssl dgst -sha256 install_path/shimx64.efi 
+openssl dgst -sha256 install_path/mmx64.efi
+openssl dgst -sha256 install_path/fbx64.efi
+
+# Create SHA256 hash files
+
+sha256sum install_path/shimx64.efi install_path/shimx64.efi.sha256
+sha256sum install_path/mmx64.efi install_path/mmx64.efi.sha256
+sha256sum install_path/fbx64.efi install_path/fbx64.efi.sha256
 
 
